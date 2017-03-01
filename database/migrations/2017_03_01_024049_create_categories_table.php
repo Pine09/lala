37bb04fb37fbe04b7550295f59f0e372951148f0
ErikJustian <justin.rikz8@gmail.com>
2017-03-01 09:51:57 +0700

    migration product update &category create
    migrate:refresh dulu

--- database/migrations/2017_03_01_024049_create_categories_table.php
+++ database/migrations/2017_03_01_024049_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->String('product_name');
-            $table->integer('category_id')->unsigned();;
-            $table->integer('qty');
-            $table->integer('price');
-            $table->string('image');
-            $table->string('description');
+            $table->string('category_name');
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 }
