34bf80ff12675ae61692f32a0d683d92e28e1ddc
pine09 <ronaldo_n09@yahoo.com>
2017-03-01 08:23:32 +0700

    naming, seeding, db changes

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ Route::get('/', function () {
 //Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function(){});
 
 
-Route::get('/potato/add', function() {
-   return view('backpack::base.addproduct');
+Route::group(['prefix' => 'potato', 'middleware' => 'admin'], function() {
+      Route::get('/add', function() {
+         return view('backpack::base.addproduct');
+      });
+      Route::get('/listproduct', 'ListController@index');
+      Route::get('/edit', function() {
+         return view('backpack::base.editproduct');
+      });
 });
-
-Route::get('/potato/listproduct', 'ListController@index');
